fc32f01f81e78a98026ea3831140b385dec6e51e
Erika Menezes <erikamenezes@Erikas-MacBook-Air.local>
2018-06-02 10:16:42 -0700

    extract pre-trained features

--- common/extract_features.py
+++ common/extract_features.py
@@ -22,12 +22,14 @@ def extract_features_white_box(DIRECTORY_PATH,
 
 def extract_features_pre_trained(DIRECTORY_PATH,
                                  model,
-                                 layer_name,
+                                 layer_name = None,
+                                 layer_index = None,
                                  includedCategories=['Dress', 'Skirt', 'UpperBody', 'LowerBody'],
                                  imageReshape = 224):
     extract_features_iterator(DIRECTORY_PATH,
                               model=model,
                               layer_name=layer_name,
+                              layer_index = layer_index,
                               includedCategories = includedCategories,
                               imageReshape = imageReshape,
                               isWhiteboxExtraction=False)
