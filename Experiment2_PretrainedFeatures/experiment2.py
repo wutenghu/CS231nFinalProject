06aafeba1ac2404d4f2b53ed37943b9328faf74e
Chaitanya Kanitkar <ckanitkar@twitter.com>
2018-06-03 15:06:48 -0700

    Delete uneeded file

--- Experiment2_PretrainedFeatures/experiment2.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-from common.extract_features import extract_features_pre_trained
-from keras.applications.vgg16 import VGG16, preprocess_input
-
-DATA_DIR = './img_npy'
-
-base_model = VGG16(weights='imagenet', include_top=True)
-extract_features_pre_trained(DATA_DIR, base_model, layer_index=-1)
