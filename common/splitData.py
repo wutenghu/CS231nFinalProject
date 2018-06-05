3fbd2ffe5e176bb1ab94078a695c1c448f44bbfb
Chaitanya Kanitkar <ckanitkar@twitter.com>
2018-06-04 20:56:57 -0700

    Changs

--- common/splitData.py
+++ common/splitData.py
@@ -1,24 +1,48 @@
 import numpy as np
+import glob
+
+DIRECTORY_PATH = "/Users/ckanitkar/Desktop/img_npy_feature_only_temp/"
+
+def splitData(dirName):
 
 
-def splitData(data_dir):
-    consumer_features = np.load(data_dir + "consumer_ResNet50_features.npy")
-    consumer_labels = np.load(data_dir + "consumer_labels.npy")
+    consumer_labels = np.load(dirName + "consumer_labels.npy")
 
-    N = consumer_features.shape[0]
+    N = consumer_labels.shape[0]
     indices = np.random.permutation(N)
-    train_idx, test_idx = indices[:int(0.7 * N)], indices[int(0.7 * N):]
-    print(len(train_idx))
+    train_idx, test_idx = indices[:int(0.8 * N)], indices[int(0.8 * N):]
 
-    print(len(test_idx))
+    assert len(train_idx) + len(test_idx) == N, "Missing some indieces"
 
-    train_consumer_features, test_consumer_features = consumer_features[train_idx], consumer_features[test_idx]
+
+    # Save Lables
+
     train_consumer_labels, test_consumer_labels = consumer_labels[train_idx], consumer_labels[test_idx]
+    np.save(dirName + "test_consumer_labels.npy", test_consumer_labels)
+    np.save(dirName + "train_consumer_labels.npy", train_consumer_labels)
 
-    np.save(data_dir + "train_consumer_ResNet50_features.npy", train_consumer_features)
-    np.save(data_dir + "train_consumer_labels.npy", train_consumer_labels)
-    np.save(data_dir + "test_consumer_ResNet50_features.npy", test_consumer_features)
-    np.save(data_dir + "test_consumer_labels.npy", test_consumer_labels)
+    # Save Features
+
+    features = ["ResNet50", "inception_v3", "vgg16", "vgg19", "whitebox"]
+    for feature in features:
+        name_tail = "consumer_{}_features.npy".format(feature)
+        filename = dirName + name_tail
+        consumer_features = np.load(filename)
+
+        assert consumer_features.shape[0] == N, "Features must have same length as labels"
+
+        train_consumer_features, test_consumer_features = consumer_features[train_idx], consumer_features[test_idx]
+
+        np.save(dirName + "train_" + name_tail, train_consumer_features)
+
+        np.save(dirName + "test_" + name_tail, test_consumer_features)
 
 
-splitData('./img_npy_final_features_only/CLOTHING/UpperBody/')
+
+
+
+categories = ["DRESSES/Dress", "DRESSES/Skirt", "CLOTHING/UpperBody", "CLOTHING/LowerBody"]
+
+for category in categories:
+    path = DIRECTORY_PATH + category + "/"
+    splitData(path)
