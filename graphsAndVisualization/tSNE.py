08b3c9b82b937be211356b51b4c8dbfd9b46ad08
Chaitanya Kanitkar <ckanitkar@twitter.com>
2018-06-05 22:24:53 -0700

    tsne

--- graphsAndVisualization/tSNE.py
+++ graphsAndVisualization/tSNE.py
@@ -4,6 +4,7 @@ from sklearn import manifold
 from time import time
 from matplotlib import offsetbox
 import itertools
+from Image_File_IO.extract_features_iterator import resizeImage
 
 # Scale and visualize the embedding vectors
 def plot_embedding(X, title=None):
@@ -22,7 +23,7 @@ def plot_embedding(X, title=None):
         shown_images = np.array([[1., 1.]])  # just something big
         for i in range(X.shape[0]):
             dist = np.sum((X[i] - shown_images) ** 2, 1)
-            if np.min(dist) <12e-3:
+            if np.min(dist) <5e-3:
                 # don't show points that are too close
                 continue
             shown_images = np.r_[shown_images, [X[i]]]
@@ -48,24 +49,48 @@ photos = np.load("/Users/ckanitkar/Desktop/photos_rgb_only/CLOTHING/LowerBody/co
 print("Computing t-SNE embedding")
 
 #t0 = time()
-# dirNames = ["DRESSES/Dress"]
-# clothingtypes = ["consumer","shop"]
+dirNames = ["DRESSES/Dress", "DRESSES/Skirt", "CLOTHING/UpperBody", "CLOTHING/LowerBody"]
+clothingtypes = ["consumer"]
+
+
+# concat = np.array([]).reshape((0, 2048))
+# concat_photos = np.array([], dtype=np.uint8).reshape((0, 3,128, 128))
+# print(concat_photos.dtype)
+# SUBSAMPLE = 400
 # for dirName, clothing in itertools.product(dirNames, clothingtypes):
 #     prefix = "/Users/ckanitkar/Desktop/img_npy_feature_only/"
 #     loadname = prefix + dirName + "/" + clothing + "_ResNet50_features.npy"
 #     print(loadname)
 #     X = np.load(loadname)
-#     print(X.shape)
-#     tsne = manifold.TSNE(n_components=2, init='pca', random_state=0, verbose=2)
-#     X_tsne = tsne.fit_transform(X)
-#     savename = "{}_embedding_{}".format(clothing, dirName.split("/")[-1])
-#     print(savename)
-#     np.save(savename, X_tsne)
+#     photo_prefix = "/Users/ckanitkar/Desktop/photos_rgb_only/"
+#     photo_name = photo_prefix + dirName + "/" + clothing + "_photos.npy"
+#     print(photo_name)
+#     photos = np.load(photo_name)
+#     print(photos.dtype)
+#     assert (photos.shape[0] == X.shape[0])
+#     sample = np.random.choice(X.shape[0], SUBSAMPLE)
+#     X_sample = X[sample, :]
+#     photo_sample = photos[sample, :, :, :]
+#
+#     # print(X_sample.shape)
+#     # print(photo_sample.shape)
+#     concat = np.concatenate((concat, X_sample))
+#     concat_photos = np.concatenate((concat_photos, photo_sample))
+#     # print(concat.shape)
+#     # print(concat_photos.shape)
+#
+#
+# tsne = manifold.TSNE(n_components=2, init='pca', random_state=0, verbose=2)
+# X_tsne = tsne.fit_transform(concat)
+# np.save("cross_category_embedding", X_tsne)
+# #
+# np.save("cross_category_photos", concat_photos)
 
-#np.save("embedding_shop", X_tsne)
-
-# ONLY USE CONSUMER EMBEDDINGS. THEY ARE MUCH BETTER
-X_tsne = np.load("consumer_embedding_LowerBody.npy")
+#ONLY USE CONSUMER EMBEDDINGS. THEY ARE MUCH BETTER
+X_tsne = np.load("cross_category_embedding.npy")
+photos = np.load("cross_category_photos.npy").transpose([0, 2,3,1])
+photos = resizeImage(photos, 64)
+print(photos.shape)
 print(X_tsne.shape)
 
 assert photos.shape[0] == X_tsne.shape[0]
