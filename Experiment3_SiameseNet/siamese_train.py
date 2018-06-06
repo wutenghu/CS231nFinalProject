72e4cbf325cfad315e7b6d9b2eda1ab725fa975f
Chaitanya Kanitkar <ckanitkar@twitter.com>
2018-06-06 00:20:54 -0700

    Checkpoint

--- Experiment3_SiameseNet/siamese_train.py
+++ Experiment3_SiameseNet/siamese_train.py
@@ -6,14 +6,20 @@ from common.computeAccuracy import computeAccuracy
 import time
 import itertools
 
-DATA_DIR = './img_npy_feature_only/DRESSES/Skirt/'
-LABELS_DIR = './labels_only/DRESSES/Skirt/'
+DATA_DIR = '/Users/ckanitkar/Desktop/img_npy_feature_only_train_test_subsample/DRESSES/Skirt/'
+SHOP_DATA_DIR = '/Users/ckanitkar/Desktop/img_npy_feature_only/DRESSES/Skirt/'
+LABELS_DIR = '/Users/ckanitkar/Desktop/labels_only/DRESSES/Skirt/'
 
-consumer_features = np.load(DATA_DIR + 'consumer_ResNet50_features.npy')
-consumer_labels = np.load(LABELS_DIR + 'consumer_labels.npy')
-shop_features = np.load(DATA_DIR + 'shop_ResNet50_features.npy')
+consumer_features = np.load(DATA_DIR + 'train_consumer_ResNet50_features.npy')
+consumer_labels = np.load(DATA_DIR + 'train_consumer_labels.npy')
+
+consumer_features = consumer_features[:200, :]
+consumer_labels = consumer_labels[:200]
+shop_features = np.load(SHOP_DATA_DIR + 'shop_ResNet50_features.npy')
 shop_labels = np.load(LABELS_DIR + 'shop_labels.npy')
 
+test_consumer_features = np.load(DATA_DIR + 'test_consumer_ResNet50_features.npy')
+test_consumer_labels = np.load(DATA_DIR + 'test_consumer_labels.npy')
 
 print (consumer_features.shape)
 print (consumer_labels.shape)
@@ -26,31 +32,47 @@ metrics = [DistanceMetrics.L1] #, DistanceMetrics.L2, DistanceMetrics.Cosine]
 lossTypes = [LossType.BinaryCrossEntropy] #LossType.SVM]
 optimizers = ['sgd'] #, 'rmsprop', 'adam']
 
+SAVE_MODEL = False
+BATCH_SIZE = 32
+EPOCHS = 2
 for metric, lossType, optimizer in itertools.product(metrics, lossTypes, optimizers):
 	print(metric, lossType, optimizer)
-	consumer_features = consumer_features[:300, :]
-	consumer_labels = consumer_labels[:300]
 
-	pair, target, _ = generatePairs(consumer_features, consumer_labels, shop_features, shop_labels, lossType = lossType)
-	distance = computeDistanceForPairs(pair, metric = metric)
-
+	# Set up model
 	input_dim = consumer_features.shape[-1]
 	hidden_dim = 2048
-
 	model = GetSiameseNet(input_dim,hidden_dim, lossType = lossType, optimizer = optimizer)
 
-	H = model.fit(distance, target, validation_split=0, epochs = 2, class_weight = {1: 500, 0: 1})
-	model_json = model.to_json()
-	model.save_weights("model_"+ str(metric)+"_"+ lossType.name +"_"+optimizer+"_"+timestr+".h5")
+	for epoch in range(EPOCHS):
+		print("Epoch", epoch)
+		num_batches = consumer_features.shape[0] // BATCH_SIZE + 1
+		batch_iter = 1
+		for start in range(0, consumer_features.shape[0], BATCH_SIZE):
+			last_index = min(consumer_features.shape[0], start + BATCH_SIZE)
+			consumer_batch = consumer_features[start: last_index]
+			consumer_labels_batch = consumer_labels[start: last_index]
 
+			pair, target, _ = generatePairs(consumer_batch, consumer_labels_batch, shop_features, shop_labels, lossType = lossType)
+			distance = computeDistanceForPairs(pair, metric = metric)
+			model.fit(distance, target, validation_split=0, epochs=1, class_weight={1: 500, 0: 1}, verbose = 0)
+
+
+			print("Finished batch {} of {}".format(batch_iter, num_batches))
+			batch_iter += 1
+
+	print("Printing train set accuracy")
 	computeAccuracy(consumer_features, shop_features, consumer_labels, shop_labels, metric=metric, model=model, k=[10, 20, 30])
 
-	#TODO: Print loss and accuracy
+	print("Printing test set acuracy")
+	computeAccuracy(test_consumer_features, shop_features, test_consumer_labels, shop_labels, metric=metric, model=model,
+					k=[10, 20, 30])
 
-	# Save model
 
-	with open("model_"+ str(metric)+"_"+ lossType.name + "_"+optimizer+"_"+timestr+".json", "w") as json_file:
-		json_file.write(model_json)
+	if (SAVE_MODEL):
+		model_json = model.to_json()
+		model.save_weights("model_" + str(metric) + "_" + lossType.name + "_" + optimizer + "_" + timestr + ".h5")
+		with open("model_"+ str(metric)+"_"+ lossType.name + "_"+optimizer+"_"+timestr+".json", "w") as json_file:
+			json_file.write(model_json)
 
 	
 
