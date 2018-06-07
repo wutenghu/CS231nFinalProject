479f928a48658d73f88b881122956c96ce64468a
Chaitanya Kanitkar <ckanitkar@twitter.com>
2018-06-06 20:33:47 -0700

    Checkpoint

--- Experiment3_SiameseNet/siamese_train.py
+++ Experiment3_SiameseNet/siamese_train.py
@@ -5,15 +5,30 @@ from common.Enums import DistanceMetrics, LossType
 from common.computeAccuracy import computeAccuracy
 import time
 import itertools
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_recall_fscore_support
+from sklearn.metrics import confusion_matrix
 
 DIR_PREFIX = "/Users/ckanitkar/Desktop/"
-CLOTHING_TYPE = "CLOTHING/LowerBody/"
+CLOTHING_TYPE = "DRESSES/Skirt/"
 FEATURE_TYPE = 'ResNet50'
-POSITIVE_CLASS_WEIGHT = 500
 
 DATA_DIR = DIR_PREFIX + 'img_npy_feature_only_train_test_subsample/' + CLOTHING_TYPE
 SHOP_DATA_DIR = DIR_PREFIX + 'img_npy_feature_only/' + CLOTHING_TYPE
 LABELS_DIR = DIR_PREFIX + 'labels_only/' + CLOTHING_TYPE
+
+
+POSITIVE_CLASS_WEIGHT = 500
+HIDDEN_DIM = 2048
+
+
+metrics = [DistanceMetrics.L1] #, DistanceMetrics.L2, DistanceMetrics.Cosine]
+lossTypes = [LossType.BinaryCrossEntropy] #LossType.SVM]
+optimizers = ['sgd'] #, 'rmsprop', 'adam']
+
+SAVE_MODEL = False
+BATCH_SIZE = 32
+EPOCHS = 2
 
 
 consumer_features = np.load(DATA_DIR + 'train_consumer_{}_features.npy'.format(FEATURE_TYPE))
@@ -34,20 +49,12 @@ print (shop_labels.shape)
 
 timestr = time.strftime("%Y%m%d-%H%M%S")
 
-metrics = [DistanceMetrics.L1] #, DistanceMetrics.L2, DistanceMetrics.Cosine]
-lossTypes = [LossType.BinaryCrossEntropy] #LossType.SVM]
-optimizers = ['adam'] #, 'rmsprop', 'adam']
-
-SAVE_MODEL = False
-BATCH_SIZE = 32
-EPOCHS = 2
 for metric, lossType, optimizer in itertools.product(metrics, lossTypes, optimizers):
 	print(metric, lossType, optimizer)
 
 	# Set up model
 	input_dim = consumer_features.shape[-1]
-	hidden_dim = 2048
-	model = GetSiameseNet(input_dim,hidden_dim, lossType = lossType, optimizer = optimizer)
+	model = GetSiameseNet(input_dim, HIDDEN_DIM, lossType = lossType, optimizer = optimizer)
 
 	for epoch in range(EPOCHS):
 		print("Epoch", epoch)
@@ -61,7 +68,14 @@ for metric, lossType, optimizer in itertools.product(metrics, lossTypes, optimiz
 			pair, target, _ = generatePairs(consumer_batch, consumer_labels_batch, shop_features, shop_labels, lossType = lossType)
 			distance = computeDistanceForPairs(pair, metric = metric)
 			negative_key = -1 if(lossType == LossType.SVM) else 0
-			model.fit(distance, target, validation_split=0, epochs=1, class_weight={1: POSITIVE_CLASS_WEIGHT, negative_key: 1}, verbose = 0)
+			model.fit(distance, target, validation_split=0, epochs=1, class_weight={1: POSITIVE_CLASS_WEIGHT, negative_key: 1}, verbose = 1)
+
+			print("Precision Recall on batch")
+			preds = model.predict(distance)
+			preds = [1 if p > 0.5 else 0 for p in preds]
+			print("accuracy on batch:", accuracy_score(target, preds))
+			print(confusion_matrix(target, preds))
+			print(precision_recall_fscore_support(target, preds, average='weighted'))
 
 
 			print("Finished batch {} of {}".format(batch_iter, num_batches))
